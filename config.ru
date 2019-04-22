d6705d46c927c3d1586e8ca1d56eec918d77ca9a
Alexei Danchenkov <alexeid@scientologymedia.net>
2019-04-22 11:20:28 -0700

    basic GET / POST requests

--- config.ru
+++ config.ru
@@ -1,6 +1,11 @@
-require_relative 'application'
+require_relative 'users_application'
+require_relative 'rides_application'
 require_relative 'database'
 
 map("/users") do 
-  run Application.new
+  run UsersApplication.new
+end
+
+map("/rides") do 
+  run RidesApplication.new
 end
--- database.rb
+++ database.rb
@@ -7,5 +7,18 @@ class Database
   def self.users
     USERS
   end
+
+  @rides = {
+    1 => { user_id: 1, title: "Morning commute", date: "2016-09-28" },
+    2 => { user_id: 1, title: "Evening commute", date: "2016-09-28" }
+  }
+
+  def self.rides
+    @rides
+  end
+
+  def self.add_ride(ride)
+    @rides[@rides.length+1] = ride
+  end
 end
 
